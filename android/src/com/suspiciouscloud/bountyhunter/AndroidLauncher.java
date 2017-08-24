3c3adae1e0eacf1376fc5ee25f1cfb4f185b222b
Shifter1996 <dusansahul@gmail.com>
2017-08-24 14:13:36 +0200

    Initial commit

---
+++ android/src/com/suspiciouscloud/bountyhunter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.suspiciouscloud.bountyhunter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.suspiciouscloud.bountyhunter.bountyHunter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new bountyHunter(), config);
+	}
+}
---
+++ desktop/src/com/suspiciouscloud/bountyhunter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.suspiciouscloud.bountyhunter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.suspiciouscloud.bountyhunter.bountyHunter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new bountyHunter(), config);
+	}
+}
